94b82010808b14ee820d8e6ed8725044eb264b97
Ali Jaffal <ajaffal121@gmail.com>
2025-01-11 18:49:33 +0200

    v1

---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.getElementById('jobsForm').addEventListener('submit', function (e) {
+  e.preventDefault(); // Prevent form from submitting
+  alert('Form submitted successfully!');
+});
+
+
+ScrollReveal().reveal('.item2', {
+  opacity: 0,
+  delay: 200,
+  distance: '100px',
+  origin: 'top',
+  duration: 1000,
+  viewFactor:0.3,
+});
+ScrollReveal().reveal('.combo-item', {
+  opacity: 0,
+  delay: 200,
+  distance: '100px',
+  origin: 'top',
+  duration: 1000,
+  viewFactor:0.3,
+
+});
+ScrollReveal().reveal('.tS', {
+  opacity: 0,
+  delay: 0,
+  distance: '10px',
+  origin: 'top',
+  duration: 2000,
+  viewFactor:0.3,
+
+});
+//jobs.html
+
+  document.getElementById('jobsForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent form from submitting
+    alert('Form submitted successfully!');
+});
