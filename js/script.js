bfb67b389aa2cee4e8b4fc6d5619ccc323bab2e8
Ali Jaffal <ajaffal121@gmail.com>
2025-01-16 10:58:17 +0200

    added about page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+ScrollReveal().reveal('.section', {
+  opacity: 0,
+  delay: 200,
+  distance: '100px',
+  origin: 'top',
+  duration: 1000,
+  viewFactor:0.3,
 
+});
 
 
 ScrollReveal().reveal('.wrapper2', {
